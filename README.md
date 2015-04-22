587836e98d2cdb60d5a56abd71114a3effb50330
weimengxi <xy441204@gmail.com>
2015-04-22 16:05:13 +0800

    init ok

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ less-test
 =========
 
 learn less
+
+- 1. 
+
+`sudo npm install `
+
+- 2. 
+`gulp init`
+
+- 3. 
+`gulp `
+
+-4. preview the page in browsers
+` http:\\localhost:3000`
--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,105 @@
-var combiner = require('stream-combiner2');
-var gulp = require('gulp');
-//var sourcemaps = require('gulp-sourcemaps');
-var less = require('gulp-less');
-var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
-var autoprefixer = require('gulp-autoprefixer');
-var ngHtml2Js = require('gulp-ng-html2js');
-var concat = require("gulp-concat");
+'use strict';
 
-/**
- * 定义静态文件路径
+/*
+ * 1. task: copy the dependencies into  from  /node_modules/**;
+ * 2. task: precomopiling the less to css;
  */
 
-var lessDir = './less';
-var cssDir = './css';
-var jsDir = "./js";
-var tplDir = "./html";
+var gulp = require('gulp'),
+    path = require('path'),
+    fs = require('fs'),
+    through = require('through'),
+    $ = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync');
 
-/**
- * 编译 masterboard LESS 文件
+var reload = browserSync.reload;
+
+var staticSrc = "static";
+var staticDist = "dist";
+
+/* 
+ * 项目初始化、或新增依赖包时，手动跑一下
+ * 把node_modules中依赖的开发包拷贝到static/vendor/目录下
  */
-var makeAppLessGlobs = function() {
-    return lessDir + "/**/*.less";
-};
-//console.log(makeAppLessGlobs("masterboard") , makeAppLessGlobs("gameboard"));
 
-gulp.task('less-css', function() {
-        gulp
-        .src(lessDir + "/**/*.less")
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(gulp.dest(cssDir + '/'));
+gulp.task('vendor', function() {
+    var conf = JSON.parse(fs.readFileSync('package.json'));
+    var depends = conf.dependencies ? (Object.keys(conf.dependencies)) : [];
+    console.log("depends", depends);
+    return gulp.src('node_modules/+(' + depends.join("|") + ')/**/*')
+        .pipe($.plumber())
+        .pipe(gulp.dest(staticDist + "/vendor"));
+
 });
 
-gulp.task('less-test',function(){
-    console.log(lessDir , cssDir);
-     gulp
-        .src("./less/**/*.less")
-        .pipe(less())
-        .pipe(gulp.dest('./css/'));
+gulp.task('images', function() {
+    return gulp.src(staticSrc + '/images/**/*')
+        .pipe(gulp.dest(staticDist + '/images'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src([staticSrc + '/fonts/**/*'])
+        .pipe(gulp.dest(staticDist + '/fonts'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('js', function() {
+    return gulp.src([staticSrc + '/js/**/*'])
+        .pipe(gulp.dest(staticDist + '/js'))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
 
-/**
- * 监听
- */
+gulp.task('html', function() {
+    return gulp.src([staticSrc + '/html/**/*'])
+        .pipe(gulp.dest(staticDist + '/html'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('less', function() {
+
+    return gulp.src([staticSrc + '/less/**/*.less'])
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.postcss([
+            require('autoprefixer-core')({
+                browsers: ['last 1 version']
+            })
+        ]))
+        .pipe(gulp.dest(staticDist + '/css'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
 gulp.task('watch', function() {
-    gulp.watch(lessDir + '/**/*.less', ['less-test']);
+
+    browserSync({
+        server: {
+            server: 'less-test',
+            baseDir: ['./'],
+            middleware: require('connect-logger')()
+        }
+    });
+
+    gulp.watch(staticSrc + '/less/**/*', ['less']);
+    gulp.watch(staticSrc + '/js/**/*', ['js']);
+    gulp.watch(staticSrc + '/images/**/*', ['images']);
+    gulp.watch(staticSrc + '/fonts/**/*', ['fonts']);
+    gulp.watch(staticSrc + '/html/**/*', ['html']);
 });
 
 
-gulp.task('default', ['less-test', 'watch']);
+//---------- gulp CLI TASK
+gulp.task('init', ['vendor', 'less', 'js', 'images', 'fonts', 'html']);
+
+gulp.task('default', ['watch']);
